0338697943a81a5494aa15bc13749185286a39d8
Omar Elawady <omarelawady1998@gmail.com>
2020-12-19 15:59:57 +0200

    add cnn model

--- fenify/train.py
+++ fenify/train.py
@@ -1,18 +1,20 @@
-from fenify.models.basic import MnistModel
+from fenify.models.cnn import CNNModel
 from fenify.prepare.generate import generate_board_sample
 from tqdm import tqdm
 from fenify.helpers.utils import flatten
+import random
+random.seed(13)
 
-train_length = 6
-test_length = 2
+train_length = 70
+test_length = 30
 
-model = MnistModel()
-model.load_latest()
+model = CNNModel()
+# model.load_latest()
 train_samples = [model.get_samples(generate_board_sample()) for _ in tqdm(range(train_length))]
 train_samples = flatten(train_samples)
 test_samples = [model.get_samples(generate_board_sample()) for _ in tqdm(range(test_length))]
 test_samples = flatten(test_samples)
-model.add_samples(test_samples)
-model.train(1000)
+model.add_samples(train_samples)
+model.train(50)
 model.evaluate(test_samples)
 model.save_snapshot()
