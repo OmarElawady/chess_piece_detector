fa26eca2f8e7a866ac93926def680ad960dfc548
Omar Elawady <omarelawady1998@gmail.com>
2020-12-22 19:31:57 +0200

    fix mouse pointer path

--- fenify/prepare/config.py
+++ fenify/prepare/config.py
@@ -1,4 +1,4 @@
-MOUSE_POINTER_IMG = "assets/hand2.png"
+MOUSE_POINTER_IMG = "assets/mouse.png"
 ARROW_LINE = '<svg opacity=".6"><marker id="arrowhead-g" orient="auto" markerWidth="4" markerHeight="8" refX="2.05" refY="2.01" cgKey="g"><path d="M0,0 V4 L3,2 Z" fill="#15781B"></path></marker><line stroke="#15781B" stroke-width="%f" stroke-linecap="round" marker-end="url(#arrowhead-g)" x1="%f" y1="%f" x2="%f" y2="%f" cgHash="b3f7green"></line></svg>'
 ARROW_CHESS_LINE = '<svg opacity="1"><marker id="arrowhead-g" orient="auto" markerWidth="4" markerHeight="8" refX="2.05" refY="2.01" cgKey="g"><path d="M0,0 V4 L3,2 Z" fill="#ffa800"></path></marker><line stroke="#ffa800" stroke-width="%f" marker-end="url(#arrowhead-g)" x1="%f" y1="%f" x2="%f" y2="%f" cgHash="b3f7green"></line></svg>'
 CIRCLE = '<svg><circle stroke="#15781B" stroke-width="%f" fill="none" opacity=".6" cx="%f" cy="%f" r="%f" cgHash="f2green"></circle></svg>'
